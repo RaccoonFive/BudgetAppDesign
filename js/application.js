e145b3cd41c5172a0e2f766e0f4c1e813f14024a
Keven Parent <kevenp@precicom.com>
2018-09-07 13:54:41 -0400

    Added side menu

--- js/application.js
+++ js/application.js
@@ -1,4 +1,10 @@
 $(document).ready(function (event) {
   $('.navbar__sideMenuButton').click(function (event) {
+    if ($('.sideBar').hasClass('active')) {
+      $('.sideBar').removeClass('active');
+    }else {
+      $('.sideBar').addClass('active');
+    }
+    
   });
 });
