f8bfe93d7832afee38ba537970abeee2fd362f22
Keven Parent <lenigma444@hotmail.com>
2018-10-17 21:38:07 -0400

    Fixed table in firefox + progress on popup

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,19 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      grid: true
+    }))
     .pipe(gulp.dest('./css'));
 });
- 
+
+
 gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
